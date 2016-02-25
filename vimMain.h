d97430b0e3083ae986a221f367597a9db6b8db58
Ron Man <ronmann32484@gmail.com>
2016-02-25 03:11:32 -0500

    Create vimMain.h
    vimMain.h

---
+++ vimMain.h
@@ -0,0 +1,318 @@
+// vimMain.h
+// Ron Mann
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class BasicMovement{
+    public:
+        // Create what the Constructor will assign and print
+BasicMovement()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                      1. BASIC MOVEMENT                                ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+
+
+    private:
+
+
+
+};
+
+class InsertionAndReplace{
+    public:
+        // Constructor
+InsertionAndReplace()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                      2. INSERTION & REPLACE                           ****" << endl;
+cout << "****                                                                       ****" << endl;
+}
+
+
+
+
+    private:
+
+};
+
+class Deletion{
+    public:
+        // Constructor
+Deletion()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                      3. DELETION                                      ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+
+
+
+    private:
+};
+
+class VisualMode{
+    public:
+        // Constructor
+VisualMode()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                      4. VISUAL MODE                                   ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+    private:
+
+
+};
+
+class InsertMode{
+    public:
+        // Constructor
+InsertMode()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                      5. INSERT MODE                                   ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+
+    private:
+
+
+};
+
+class Copying{
+    public:
+        // Constructor
+Copying()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                      6. COPYING                                       ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+    private:
+
+
+};
+
+class AdvancedInsertion{
+    public:
+        // Constructor
+AdvancedInsertion()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                      7. ADVANCED INSERTION                            ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+
+    private:
+
+
+
+
+};
+
+class UndoingAndRepeating{
+    public:
+        // Constructor
+UndoingAndRepeating()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                      8. UNDOING & REPEATING                           ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+
+
+    private:
+
+
+};
+
+class ComplexMovement{
+    public:
+        // Constructor
+ComplexMovement()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                      9. COMPLEX MOVEMENT                              ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+
+    private:
+
+
+
+
+};
+
+class SearchAndSubstitution{
+    public:
+        // Constructor
+SearchAndSubstitution()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                    10. SEARCH & SUBSTITUTION                          ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+
+
+
+
+
+
+};
+
+class SpecialCharacters{
+    public:
+        // Constructor
+SpecialCharacters()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                    11. SPECIAL CHARACTERS                             ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+    private:
+
+
+};
+
+class OffsetInSearchCommands{
+    public:
+        // Constructor
+OffsetInSearchCommands()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                    12. OFFSET IN SEARCH COMMANDS                      ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+    private:
+
+
+};
+
+class MarksAndMotions{
+    public:
+        // Constructor
+MarksAndMotions()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                    13. MARKS & MOTIONS                                ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+    private:
+
+
+};
+
+class Tags{
+    public:
+        // Constructor
+Tags()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                    14. TAGS                                           ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+    private:
+
+
+
+};
+
+class ScrollingAndMultiWindowing{
+    public:
+        // Constructor
+ScrollingAndMultiWindowing()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                    15. SCROLLING & MULTIWINDOWING                     ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+    private:
+
+
+
+};
+
+class KeyMapping_Abbreviations{
+    public:
+        // Constructor
+KeyMapping_Abbreviations()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                    16. KEYMAPPING ABBREVIATIONS                       ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+    private:
+
+
+};
+
+class ExRanges{
+    public:
+        // Constructor
+ExRanges()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                    17. EXRANGES                                       ****" << endl;
+cout << "****                                                                       ****" << endl;
+
+}
+
+    private:
+
+
+};
+
+class Misc{
+    public:
+        // Constructor
+Misc()
+{
+cout << "****                                                                       ****" << endl;
+cout << "****                    18. MISC                                           ****" << endl;
+cout << "****                                                                       ****" << endl;
+cout << "****                                                                       ****" << endl;
+cout << "*******************************************************************************" << endl;
+cout << "*******************************************************************************" << endl;
+cout << "*******************************************************************************" << endl;
+cout << "*******************************************************************************" << endl;
+
+}
+
+    private:
+
+
+};
